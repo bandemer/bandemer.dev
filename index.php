98db679faeda78f128a40a3beb3c0a30d0884471
Matthias Bandemer <git@bandemer.net>
2019-12-07 00:06:48 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>Matthias Bandemer - Webentwickler</title>
+<link href="assets/style.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="container-1">
+    <h1 class="item-1">Matthias Bandemer - Webentwickler</h1>
+    <div class="item-2">
+        <p><span class="first-word">Hallo,</span> mein Name ist Matthias Bandemer und ich bin ein Webentwickler aus Dresden.</p>
+        <p>Mein Herz schlägt für PHP, Frameworks, Rest APIs, #TDD und #DevOps.</p>
+    </div>
+</div>
+
+<div class="container-2">
+    <h2 class="item-3">Meine Kontaktdaten:</h2>
+    <div class="item-4"><a href="mailto:matthias@bandemer.net" class="link-mail" title="E-Mail an mich senden">matthias@bandemer.net</a></div>
+    <div class="item-5"><a href="https://github.com/bandemer" class="link-github" title="Mein Profil auf GitHub">github.com/bandemer</a></div>
+    <div class="item-6"><a href="assets/matthias-bandemer.vcf" class="link-vcard" title="Meine Kontaktdaten als vCard-Datei"><span>Download </span>vCard</a></div>
+</div>
+
+</body>
+</html>
+
