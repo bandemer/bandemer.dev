bd3298b22021a8b11d8eeed145065aabf5993900
Matthias Bandemer <git@bandemer.net>
2020-01-12 12:14:48 +0100

    Added Open Graph info

--- index.php
+++ index.php
@@ -6,15 +6,20 @@
 <title>Matthias Bandemer - Webentwickler</title>
 <link href="assets/style.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=swap" rel="stylesheet">
-<meta property="og:title" content="Matthias Bandemer - Webentwickler"/>
-<meta property="og:description" content="Hi, mein Name ist Matthias Bandemer und ich bin ein Webentwickler aus Dresden."/>
-<meta property="og:type" content="website"/>
-<meta property="og:url" content="https://bandemer.dev"/>
-<meta property="og:site_name" content="Matthias Bandemer - Webentwickler"/>
-<meta property="og:image" content="https://bandemer.dev/assets/matthias-bandemer.png"/>
+<link rel="shortcut icon" href="/favicon.ico">
+<link rel="icon" type="image/png" href="/favicon.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/favicon.png" sizes="96x96">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<meta name="msapplication-TileColor" content="#EB5160">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta property="og:title" content="Matthias Bandemer - Webentwickler">
+<meta property="og:description" content="Hi, mein Name ist Matthias Bandemer und ich bin ein Webentwickler aus Dresden.">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://bandemer.dev">
+<meta property="og:site_name" content="Matthias Bandemer - Webentwickler">
+<meta property="og:image" content="https://bandemer.dev/assets/matthias-bandemer.png">
 </head>
 <body>
-
 <div class="container-1">
     <h1 class="item-1">Matthias Bandemer - Webentwickler</h1>
     <div class="item-2">
@@ -22,13 +27,11 @@
         <p>Mein Herz schlägt für PHP, Frameworks, Rest APIs, #TDD und #DevOps.</p>
     </div>
 </div>
-
 <div class="container-2">
     <h2 class="item-3">Meine Kontaktdaten:</h2>
     <div class="item-4"><a href="mailto:matthias@bandemer.net" class="link-mail" title="E-Mail an mich senden">matthias@bandemer.net</a></div>
     <div class="item-5"><a href="https://github.com/bandemer" class="link-github" title="Mein Profil auf GitHub">github.com/bandemer</a></div>
     <div class="item-6"><a href="assets/matthias-bandemer.vcf" class="link-vcard" title="Meine Kontaktdaten als vCard-Datei"><span>Download </span>vCard</a></div>
 </div>
-
 </body>
 </html>
