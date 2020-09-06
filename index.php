9dfe88fdd1c92fa411576ed9fc9af7dbd4cc3faf
Matthias Bandemer <git@bandemer.net>
2020-09-06 16:26:20 +0200

    Footer hinzugefügt

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Matthias Bandemer - Webentwickler</title>
-<link href="assets/style.css" rel="stylesheet">
+<link href="assets/style.css?date=<?php echo date('YmdHis', filemtime('assets/style.css')); ?>" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=swap" rel="stylesheet">
 <link rel="shortcut icon" href="/favicon.ico">
 <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32">
@@ -33,5 +33,9 @@
     <div class="item-5"><a href="https://github.com/bandemer" class="link-github" title="Mein Profil auf GitHub">github.com/bandemer</a></div>
     <div class="item-6"><a href="assets/matthias-bandemer.vcf" class="link-vcard" title="Meine Kontaktdaten als vCard-Datei"><span>Download </span>vCard</a></div>
 </div>
+<div class="footer">
+    <a href="https://pdf-barrierefrei.bandemer.net" class="link-external">Barrierefreie PFD-Dateien</a>
+</div>
+
 </body>
 </html>
