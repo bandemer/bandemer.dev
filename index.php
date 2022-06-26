21a3cf3c6065d89b367dd23440766e0519ef2fbe
Matthias Bandemer <git@bandemer.net>
2022-06-26 15:46:27 +0200

    Minor fixes for meta tags

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Matthias Bandemer - Webentwickler</title>
+<meta name="description" content="Hi, mein Name ist Matthias Bandemer und ich bin ein Webentwickler aus Dresden.">
+<meta name="robots" content="index,follow">
+<link rel="canonical" href="https://bandemer.dev/">
 <link href="assets/style.css?date=<?php echo date('YmdHis', filemtime('assets/style.css')); ?>" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro&display=swap" rel="stylesheet">
+<link href="assets/fonts/source-serif-pro/index.css" rel="stylesheet">
 <link rel="shortcut icon" href="/favicon.ico">
 <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32">
 <link rel="icon" type="image/png" href="/favicon.png" sizes="96x96">
@@ -32,9 +35,6 @@
     <div class="item-4"><a href="mailto:matthias@bandemer.net" class="link-mail" title="E-Mail an mich senden">matthias@bandemer.net</a></div>
     <div class="item-5"><a href="https://github.com/bandemer" class="link-github" title="Mein Profil auf GitHub">github.com/bandemer</a></div>
     <div class="item-6"><a href="assets/matthias-bandemer.vcf" class="link-vcard" title="Meine Kontaktdaten als vCard-Datei"><span>Download </span>vCard</a></div>
-</div>
-<div class="footer">
-    <a href="https://pdf-barrierefrei.bandemer.net" class="link-external">Barrierefreie PDF-Dateien</a>
 </div>
 
 </body>
